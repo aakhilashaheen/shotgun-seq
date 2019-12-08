0cfc5303f9fdb4e16b303aa03c2a35f5238d11b3
Aakhila Shaheen <shahe026@umn.edu>
2019-12-07 22:45:53 -0600

    Updates

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     # Read the original file and subsample n random reads using subsample.py
     vertices, edges = utils.process_samples("sample.txt", 5)
     print("------------EDGES-------------")
+    
     print(edges)
     print("-----------VETICES-------------")
     print(vertices)
--- utils.py
+++ utils.py
@@ -13,10 +13,59 @@ def process_samples(file, k):
             # Finding all k-mers from that read and making a non-unique []
             for i in range(len(line)-k+1):
                 kmers_repeats.append(line[i:i+k])
-                
         vertices, edges = graph_euler(kmers_repeats, k)
 
+    # line = 'GACGGCGGCGCACGGCGCAA'
+    # for i in range(len(line)-k+1):
+    #     kmers_repeats.append(line[i:i+k])   
+
     return vertices, edges
+
+# Calculates the in and out degrees of each vertex
+def degrees(edges, vertices):
+    inDegree = {}
+    outDegree = {}
+
+    # Set default in and out as 1 for all the vertices:
+    for vert in vertices.values():
+        inDegree[vert] = 1
+        outDegree[vert] = 1
+
+
+    for edge in edges:
+        src = edge[0]
+        dst = edge[1]
+        if src in outDegree.keys():
+            outDegree[src] = outDegree.get(src) + 1
+        else:
+            outDegree[src] = 1
+        if dst in inDegree.keys():
+            inDegree[dst] = inDegree.get(dst) + 1
+        else:
+            inDegree[dst] = 1
+    return inDegree, outDegree
+
+def verify(inDegree, outDegree, vertices):
+    start = 0
+    end = 0
+    for vert in vertices.values():
+        ins = inDegree.get(vert)
+        outs = outDegree.get(vert)
+        print(vert, ins, outs)
+        if (ins == outs):
+            continue
+        elif (ins - outs == 1):
+            end = vert
+        elif (outs - ins == 1):
+            start = vert
+        else:
+            start, end = -1, -1
+            break
+    if (start >= 0) and (end >= 0):
+        print("Gooodddd")
+        return start
+    else:
+        return -1
 
 
 # Get vertices and edges for euler path
@@ -40,11 +89,17 @@ def graph_euler(kmers_repeats,k):
     # Numeric labels for nodes for easy graph traversal
     for i,node in enumerate(nodes):
         vertices[node] = i
+
+    print(vertices)
         
     # Adding the kmer as the edge for the graph between suffix and prefix nodes
     for kmer1 in kmers_repeats:
         start = kmer1[:k-1]
         end = kmer1[1:k]
         edges.append([vertices[start], vertices[end]])
+
+    # Sanity check
+    inDegree, outDegree = degrees(edges, vertices)
+    print(verify(inDegree, outDegree, vertices))
 
     return vertices, edges
